8df46fa29c4935379fddd5c8c1853db97a4b0cbf
Ignitex00G <gabrielappscoordination.01@gmail.com>
2025-07-12 18:15:55 -0400

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'neo-screen-cache-v1';
+const OFFLINE_URL = 'index.html';
+const PRECACHE_RESOURCES = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'css/design/global.css',
+  'join/join.js',
+  'contact/contact.js',
+  'mychatbot/chatbot.js',
+  'expand-nav/nav.js',
+  'js/funct/index.js',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_RESOURCES))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) {
+        return cached;
+      }
+      return fetch(event.request).catch(() => caches.match(OFFLINE_URL));
+    })
+  );
+});
